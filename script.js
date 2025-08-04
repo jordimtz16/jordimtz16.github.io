64029aa6728acbc0242143f3748cc893c74030da
Jordi Martinez Munoz <132799007+jordimtz16@users.noreply.github.com>
2025-08-04 03:56:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // Optional interactivity
 console.log("Welcome to your personal site!");
+
+
+function openTab(evt, tabName) {
+  const tabContents = document.querySelectorAll(".tab-content");
+  const tabLinks = document.querySelectorAll(".tab-link");
+
+  tabContents.forEach(tab => tab.classList.remove("active"));
+  tabLinks.forEach(btn => btn.classList.remove("active"));
+
+  document.getElementById(tabName).classList.add("active");
+  evt.currentTarget.classList.add("active");
+}
